e313c946afd703de519db69495affeb9bcae1038
Ricardo Alonzo <Alonzoricardo6@gmail.com>
2019-12-03 21:40:44 +0100

    speeding fibonacci

--- lib/fibonacci/fibonacci.ex
+++ lib/fibonacci/fibonacci.ex
@@ -13,14 +13,12 @@ defmodule AlgosAdvanced.Fibonacci.Memoized do
   @moduledoc """
   Linear time O(n)
   """
-  def calculateFibAt(n), do: calculateFibAt(n, [0, 1], 2)
+  def calculateFibAt(n), do: calculateFibAt(n, [1, 0], 2)
 
-  def calculateFibAt(n, seq, c) when c > n, do: seq
+  def calculateFibAt(n, [fib | seq], c) when c > n, do: fib
 
-  def calculateFibAt(n, seq, c) do
-    fib1 = Enum.at(seq, c - 1)
-    fib2 = Enum.at(seq, c - 2)
-    calculateFibAt(n, seq ++ [fib1 + fib2], c + 1)
+  def calculateFibAt(n, [fib1 | [fib2 | seq]], c) do
+    calculateFibAt(n, [fib1 + fib2, fib1, fib2 | seq], c + 1)
   end
 end
 
@@ -35,4 +33,22 @@ defmodule AlgosAdvanced.Fibonacci.ConstantSpace do
   def calculateFibAt(n, c, fib1, fib2) do
     calculateFibAt(n, c + 1, fib2, fib2 + fib1)
   end
+end
+
+defmodule AlgosAdvanced.Fibonacci.Iterative do
+  def calculateFibAt(n) do
+    {_, fib} =
+      Enum.reduce(2..n, {0, 1}, fn _x, acc ->
+        {fib1, fib2} = acc
+        {fib2, fib1 + fib2}
+      end)
+
+    fib
+  end
+
+  # def calculateFibAt(n, c, fib1, fib2) when c == n, do: fib1 + fib2
+
+  # def calculateFibAt(n, c, fib1, fib2) do
+  #   calculateFibAt(n, c + 1, fib2, fib2 + fib1)
+  # end
 end
--- lib/fibonacci_benchmark.exs
+++ lib/fibonacci_benchmark.exs
@@ -1,10 +1,16 @@
 Benchee.run(
   %{
-    "Recursive" => fn fib -> AlgosAdvanced.Fibonacci.Recursive.calculateFibAt(fib) end,
+    # "Recursive" => fn fib -> AlgosAdvanced.Fibonacci.Recursive.calculateFibAt(fib) end,
     "Memoized" => fn fib -> AlgosAdvanced.Fibonacci.Memoized.calculateFibAt(fib) end,
     "ConstantSpace" => fn fib ->
       AlgosAdvanced.Fibonacci.ConstantSpace.calculateFibAt(fib)
-    end
+    end,
+    "Iterative" => fn fib -> AlgosAdvanced.Fibonacci.Iterative.calculateFibAt(fib) end
   },
-  inputs: [{:fib_8, 8}, {:fib_12, 12}, {:fib_16, 16}]
+  inputs: [
+    {:fib_8, 8},
+    {:fib_12, 12},
+    {:fib_16, 16}
+    # {:fib, 10000}
+  ]
 )
